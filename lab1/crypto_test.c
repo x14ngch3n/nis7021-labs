94b6499f9fdb0635e6eba2eabcc7676185498d14
cascades-sjtu <cascades-sjtu@users.noreply.github.com>
2022-10-10 10:18:38 +0000

    deploy: 79563bd8a13f6c8cca1f4f1aa481819f88d43c96

--- lab1/crypto_test.c
+++ lab1/crypto_test.c
@@ -11,12 +11,12 @@ int cyrpto_test() {
   char buf[1024];
   int pass = 1;
 
-  // Aspply `encode` function from libcrytpo.a
+  // Apply `encode` function from libcrytpo.a
   strcpy(buf, text);
   encode(buf);
   pass = pass && !strcmp(code, buf);
 
-  // Apple `decode` function from libcrytpo.a
+  // Apply `decode` function from libcrytpo.a
   decode(buf);
   pass = pass && !strcmp(text, buf);
 
